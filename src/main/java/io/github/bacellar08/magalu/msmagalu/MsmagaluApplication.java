c8330dc4cb1d11365d9cc08df97165c4ad7e2234
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-07-05 23:31:02 -0300

    feat: configuração do banco de dados pelo docker

---
+++ src/main/java/io/github/bacellar08/magalu/msmagalu/MsmagaluApplication.java
@@ -0,0 +1,13 @@
+package io.github.bacellar08.magalu.msmagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bacellar08/magalu/msmagalu/MsmagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bacellar08.magalu.msmagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
